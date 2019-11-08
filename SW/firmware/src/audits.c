d30e8ddf6151e9277c25923cbb8b9a3951cff5a0
Rachid AKKOUCHE <rachid.akkouche@wanadoo.fr>
2019-11-08 17:29:18 +0100

    EEprom OK
    Signed-off-by: Rachid AKKOUCHE <rachid.akkouche@wanadoo.fr>

---
+++ SW/firmware/src/audits.c
@@ -0,0 +1,366 @@
+/***************************************************************************
+ *  \author Rachid AKKOUCHE
+ * 
+ *  Company RASoftware
+ * 
+ * \date 2019 11 018
+ * 
+ * \file audits.c
+ * 
+ * \brief Fichier gestion des audits.
+ * 
+ * \details Gestion des audits
+ *  
+ ***************************************************************************/
+/* ************************************************************************** */
+
+#include <string.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "mdb_bv.h"
+#include "mdb_cg.h"
+#include "globaldef.h"
+#include "config/../configuration.h"
+#include "driver/at24/drv_at24.h"
+#include "peripheral/gpio/plib_gpio.h"
+
+/**
+ * \addtogroup audits
+ * @{
+ */
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Included Files                                                    */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/* This section lists the other files that are included in this file.
+ */
+
+/* TODO:  Include other files here if needed. */
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Constants                                                         */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/**
+ * \brief Delay de la tâche
+ */
+#define AUDITS_DELAY (1000 * MILLISEC)
+
+/**
+ * \brief Valeur indiquant que l'eeprom a été initialis'.
+ */
+#define AUDITS_USED_FLAG 0XA5A5A5A5
+
+/**
+ * \brief Nom de la tâche.
+ */
+#define AUDITS_TASK_NAME "Audits"
+
+/**
+ * \brief Profondeur du tas de la tâche.
+ */
+#define AUDITS_TASK_STACK 512
+
+/**
+ * 
+ * \brief Priorité de la tâche.
+ */
+#define AUDITS_TASK_PRIORITY 2
+
+/**
+ * \brief Nombre d'éléments dans la queue.
+ */
+#define AUDITS_QUEUE_LEN 8
+
+/**
+ * \brief Adresse du flag d'initialisation dans l'eeprom
+ */
+//#define ADDRESS_FLAG (DRV_AT24_EEPROM_FLASH_SIZE - sizeof(uint32_t))
+#define ADDRESS_FLAG (1024 - sizeof(uint32_t))
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: File Scope or Global Data                                         */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+
+/**
+ * \brief Structure contenant les données d'un enregistrement à sauvegarder dans 
+ * l'eeprom.
+ */
+typedef struct
+{
+    uint32_t dwAddress; /*!<Adresse à laquelle sera enregistrée la valeur.*/
+    uint32_t dwValue; /*<Valeur de la donnée à sauvegarder.*/
+} RECORDWORD;
+
+/**
+ * \brief Etats de la tâche des audits.
+ */
+typedef enum
+{
+    AUDITS_STATE_INIT, /*!<Initialisation des audits.*/
+    AUDITS_STATE_READ, /*!<Lecture des audits en eeprom. Si le flag 
+                        * d'initialisation n'a pas la valeur 0xA5A5A5A5, les
+                        * audits seront  remis à zéro.*/
+    AUDITS_STATE_IDLE, /*!<Aucune opération requise sur les audits.*/
+    AUDITS_STATE_CLEAR, /*!<Enregistre 0xFFFFFFFF pour le flag d'initialisation
+                         * et positionne l'état en mode lecture pour provoquer 
+                         * la remise à zéro des audits. */
+    AUDITS_STATE_NUM, /*!<Nombre d'état.*/
+} AUDITS_STATES;
+
+/**
+ * \brief Structure des datas d'audits
+ */
+typedef struct
+{
+    uint32_t dwCG_in[CG_NUM_CHANNELS]; /*!<Tableau du nombre de pièces par canal 
+                                        * acceptées par le rendeur.*/
+    uint32_t dwCG_OUT[CG_NUM_CHANNELS]; /*!<<Tableau du nombre de pièces par 
+                                         * canal retournées par le rendeur.*/
+    uint32_t dwBV_in[BV_NUM_CHANNELS]; /*!<Tableau du nombre de billets par 
+                                        * canal acceptés par le lecteur de 
+                                        * billets.*/
+    uint32_t dwProductsOut[PRODUCT_NUMBER]; /*!<Tableau du nombre de produits 
+                                             * distribués.*/
+    uint32_t dwOverPay; /*!<Montant du trop perçu en cts.*/
+} SAUDITS;
+
+/**
+ * \brief Union contenant les audits.
+ */
+typedef union
+{
+    uint8_t buffer[sizeof(SAUDITS) / DRV_AT24_EEPROM_PAGE_SIZE * DRV_AT24_EEPROM_PAGE_SIZE + DRV_AT24_EEPROM_PAGE_SIZE]; /*!<Tableau contenant les données audits.*/
+    SAUDITS saudit; /*<Structure contenant les données audits.*/
+} UAUDITS;
+
+/**
+ * \brief Variables contenant les données de la gestion des audits.
+ */
+static struct
+{
+    AUDITS_STATES state; /*!<Etat de la tâche.*/
+    DRV_HANDLE hDrvAT24; /*!<Handle du driver de la mémoire eeprom*/
+    QueueHandle_t hAuditQueue; /*!<Handle de la queue utilisée comme liste des 
+                                * informations à enregistrer.*/
+    TaskHandle_t hAuditHandle; /*!<Handle de la tâche de la machine d'état 
+                                * utilisée par le module.*/
+    UAUDITS dataBuffer; /*!<Buffer contenant les donneées d'audits.*/
+    RECORDWORD record; /*!<Variable temporaire utilisée pour enregistrer les 
+                        * informations dans l'eeprom.*/
+} audits;
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+// Section: Local Functions                                                   */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+
+/*********************************************************************
+ * Function:        
+ *         static void vTaskAudit(void *vParameters)
+ * 
+ * Author:
+ *         Rachid AKKOUCHE
+ * 
+ * Date:
+ *         19/11/08
+ *
+ * Summary:
+ *         Tâche du module de gestion des audits.
+ * 
+ * Description:
+ *         Cette tâche freertos effectue en permanence la vérification de l'état
+ * de la machine d'état du module et effectue les opérations correspondant à 
+ * l'état en cours.
+ *
+ * PreCondition:    
+ *         La tâche doit avoir été crée par la fonction vAuditsInit et le 
+ * scheduler FreeRtos actif.
+ *
+ * Input:     
+ *         None
+ *
+ * Output:
+ *         None
+ *
+ * Returns:
+ *         None
+ *
+ * Side Effects:
+ *         None
+ * 
+ * Example:
+ *         <code>
+ *         FUNC_NAME(FUNC_PARAM)
+ *         <code>
+ * 
+ * Remarks:
+ *         None
+ *         
+ ********************************************************************/
+static void vTaskAudit(void *vParameters)
+{
+    int index = 0;
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    while(1)
+    {
+        switch(audits.state)
+        {
+            case AUDITS_STATE_INIT:
+                // <editor-fold desc="AUDIT_INIT"> 
+            {
+                if((DRV_AT24_Status(DRV_AT24_INDEX) == SYS_STATUS_READY) &&
+                   ((audits.hDrvAT24 = DRV_AT24_Open(DRV_AT24_INDEX, DRV_IO_INTENT_READWRITE)) !=
+                    DRV_HANDLE_INVALID))
+                {
+                    audits.state = CLR_Get() ? AUDITS_STATE_READ : AUDITS_STATE_CLEAR;
+                    audits.hAuditQueue = xQueueCreate(AUDITS_QUEUE_LEN, sizeof( RECORDWORD));
+                }
+                break;
+            }// </editor-fold>
+            case AUDITS_STATE_READ:
+                // <editor-fold desc="AUDITS_READ"> 
+            {
+                audits.state = AUDITS_STATE_IDLE;
+                DRV_AT24_Read(audits.hDrvAT24, &audits.record.dwValue,
+                              sizeof(audits.record.dwValue), ADDRESS_FLAG);
+                while(DRV_AT24_TransferStatusGet(audits.hDrvAT24) == DRV_AT24_TRANSFER_STATUS_BUSY);
+                if(audits.record.dwValue != AUDITS_USED_FLAG)
+                {
+                    memset(&audits.dataBuffer, 0, sizeof(UAUDITS));
+                    for(index = 0; index < (sizeof(UAUDITS) / DRV_AT24_EEPROM_PAGE_SIZE); index++)
+                    {
+                        DRV_AT24_PageWrite(audits.hDrvAT24, &audits.dataBuffer, index * DRV_AT24_EEPROM_PAGE_SIZE);
+                        while(DRV_AT24_TransferStatusGet(audits.hDrvAT24) == DRV_AT24_TRANSFER_STATUS_BUSY);
+                    }
+                    audits.record.dwAddress = ADDRESS_FLAG;
+                    audits.record.dwValue = AUDITS_USED_FLAG;
+                    xQueueSendToBack(audits.hAuditQueue, &audits.record, 1000);
+                }
+                else
+                {
+                    DRV_AT24_Read(audits.hDrvAT24, audits.dataBuffer.buffer, sizeof(UAUDITS), 0);
+                    while(DRV_AT24_TransferStatusGet(audits.hDrvAT24) == DRV_AT24_TRANSFER_STATUS_BUSY);
+                }
+                break;
+            }// </editor-fold>
+            case AUDITS_STATE_IDLE:
+                // <editor-fold desc="AUDIT_IDLE"> 
+            {
+
+                if(xQueueReceive(audits.hAuditQueue, &audits.record, 10))
+                {
+                    DRV_AT24_Write(audits.hDrvAT24, &audits.record.dwValue,
+                                   sizeof(audits.record.dwValue),
+                                   audits.record.dwAddress);
+                    while(DRV_AT24_TransferStatusGet(audits.hDrvAT24) == DRV_AT24_TRANSFER_STATUS_BUSY);
+                    if(audits.record.dwAddress == ADDRESS_FLAG)
+                    {
+                        if(audits.record.dwValue == UINT32_MAX)
+                        {
+                            audits.state = AUDITS_STATE_READ;
+                        }
+                        if(audits.record.dwValue == AUDITS_USED_FLAG)
+                        {
+                            LED_SYS_Set();
+                            while(1);
+                        }
+                    }
+                }
+                break;
+            }// </editor-fold>
+            case AUDITS_STATE_CLEAR:
+                // <editor-fold desc="AUDITS-STATE_C"> 
+            {
+                audits.state = AUDITS_STATE_IDLE;
+                audits.record.dwAddress = ADDRESS_FLAG;
+                audits.record.dwValue = UINT32_MAX;
+                xQueueSendToBack(audits.hAuditQueue, &audits.record, 1000);
+                break;
+            }// </editor-fold>
+            default:
+            {
+
+                break;
+            }
+        }
+        vTaskDelayUntil(&xLastWakeTime, AUDITS_DELAY);
+    }
+}
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+// Section: Interface Functions                                               */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/*********************************************************************
+ * Function:        
+ *         void vAuditsInit(void)
+ * 
+ * Author:
+ *         Rachid AKKOUCHE
+ * 
+ * Date:
+ *         19/11/01
+ *
+ * Summary:
+ *         Initialisation du module
+ * 
+ * Description:
+ *         Position l'état de la machine d'état sur AUDIT_STATE_INIT pour
+ * permettre la création du handle de la mémoire I2C. Puis crée la tâche pour
+ * FreeRtos du module.
+ *
+ * PreCondition:    
+ *         None
+ *
+ * Input:     
+ *         None
+ *
+ * Output:
+ *         None
+ *
+ * Returns:
+ *         None
+ *
+ * Side Effects:
+ *         None
+ * 
+ * Example:
+ *         <code>
+ *         FUNC_NAME(FUNC_PARAM)
+ *         <code>
+ * 
+ * Remarks:
+ *         None
+ *         
+ ********************************************************************/
+void vAuditsInit(void)
+{
+    if(audits.hAuditHandle == NULL)
+    {
+        audits.state = AUDITS_STATE_INIT;
+        xTaskCreate(vTaskAudit, AUDITS_TASK_NAME, AUDITS_TASK_STACK, NULL,
+                    AUDITS_TASK_PRIORITY, &audits.hAuditHandle);
+    }
+}
+
+/**
+ * @}
+ */
+/* *****************************************************************************
+End of File
+ */
---
+++ SW/firmware/src/globaldef.h
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/**
+ * \author Rachid AKKOUCHE
+ * 
+ *  Company RASoftware
+ * 
+ * \date 2019 11 01
+ * 
+ * \file globaldef.h
+ * 
+ * \brief Fichier des definitions utilisées dans le programme.
+ * 
+ * \details
+ *  
+ ***************************************************************************/
+
+#ifndef _GLOBAL_DEF_H    /* Guard against multiple inclusion */
+#define _GLOBAL_DEF_H
+
+/**
+ * \defgroup globals GLOBALS
+ * Definitions et variables globales.
+ * @{
+ */
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Included Files                                                    */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/* This section lists the other files that are included in this file.
+ */
+
+/* TODO:  Include other files here if needed. */
+#include "FreeRTOSConfig.h"
+
+/* Provide C++ Compatibility */
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+    /* ************************************************************************** */
+    /* ************************************************************************** */
+    /* Section: Constants                                                         */
+    /* ************************************************************************** */
+    /* ************************************************************************** */
+    
+    /**
+     * \brief Nombre de produits.
+     */
+#define PRODUCT_NUMBER 3
+    
+    /**
+     * \brief Provoque un délai de x milliseconde.
+     */
+#define delayMs(x) vTaskDelay(pdMS_TO_TICKS(x))
+
+    /**
+     * \brief Fréquence d'horloge du CPU.
+     */
+#define GetSystemClock() configCPU_CLOCK_HZ
+
+    /**
+     * \brief Définition d'une seconde pour Freertos
+     */
+#define SECONDE pdMS_TO_TICKS(1000)
+
+    /**
+     * \brief Définition d'une milliseconde pour Freertos.
+     */
+#define MILLISEC pdMS_TO_TICKS(1)
+
+
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: Data Types
+    // *****************************************************************************
+    // *****************************************************************************
+
+    // *****************************************************************************
+    // *****************************************************************************
+    // Section: Interface Functions
+    // *****************************************************************************
+    // *****************************************************************************
+
+    /*  A brief description of a section can be given directly below the section
+        banner.
+     */
+
+/**
+ @}
+ */
+    /* Provide C++ Compatibility */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*  */
+
+/* *****************************************************************************
+ End of File
+ */
